10566a94fdcd7c1eb5815ff2ddea74bcdba04f50
beta_astra <kiran.211720@gmail.com>
2024-04-14 17:07:29 +0545

    navigation drawer and bottom navigation added

--- app/src/main/java/com/example/myquiz/DashboardActivity.kt
+++ app/src/main/java/com/example/myquiz/DashboardActivity.kt
@@ -1,16 +1,51 @@
 package com.example.myquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.myquiz.databinding.ActivityDashboardBinding
+import com.example.myquiz.databinding.ActivityMainBinding
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+    private lateinit var ImageDrawer : ImageButton
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = binding.textView.text.toString()
+        val email = binding.textView.text.toString()
+        val mail = binding.textView.text.toString()
+        val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        val check = sharedPref.getBoolean("flag", true)
+        val Username = sharedPref.getString("name", name)
+        val emails = sharedPref.getString("email",email)
+
+
+
+        ImageDrawer = findViewById(R.id.navigationdrawer)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        ImageDrawer.setOnClickListener {
+            drawerLayout.open()
+
+        }
+
+        binding.textView.text = "$Username"
+//        emai.setText("$emails")
+//        user.setText("$user")
 
     }
+
 }
+
+
--- app/src/main/java/com/example/myquiz/MainActivity.kt
+++ app/src/main/java/com/example/myquiz/MainActivity.kt
@@ -3,25 +3,45 @@ package com.example.myquiz
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.myquiz.databinding.ActivityDashboardBinding
 import com.example.myquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var nextTo : Button
+    private lateinit var nextTo: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        nextTo = findViewById(R.id.Button)
-        nextTo.setOnClickListener{
-            val intent = Intent(this,DashboardActivity::class.java)
-            startActivity(intent)
-        }
 
+        val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+        val check = sharedPref.getBoolean("flag", false)
+        val editor = sharedPref.edit()
+
+        nextTo = findViewById(R.id.Button)
+
+        if (check) {
+            val intent = Intent(this@MainActivity, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            nextTo.setOnClickListener {
+                val name = binding.name.text.toString()
+                val email = binding.email.text.toString()
+                editor.apply {
+                    putBoolean("flag", true)
+                    putString("name", name)
+                    putString("email",email)
+                    apply()
+                }
+
+                    val intent = Intent(this@MainActivity, DashboardActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
         }
     }
+
